989943166540a09f0afbc935608d863e233badcd
unknown <river@inbox.ru>
2021-07-04 18:59:05 +0300

    040721

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
-import Header from './components/Header';
-import Profile from './components/Profile';
-import Nav from './components/Nav';
+import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
