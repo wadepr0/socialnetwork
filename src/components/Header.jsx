8674901fdd365f3916a8da9eedda75733563489c
unknown <river@inbox.ru>
2021-07-04 16:14:32 +0300

    040721

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <img src="/images/reactjs-icon.svg" alt="" />
+            <div className="logo">WADE<span>PRO</span></div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav className="nav">
+            <ul>
+                <li><a href="#">profile</a></li>
+                <li><a href="#">messages</a></li>
+                <li><a href="#">likes</a></li>
+                <li><a href="#">settings</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
